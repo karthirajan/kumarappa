411d9dae8f149d120d32e4b1e964ffe1893fc78c
kathiS1007 <karthick@centricsoftware.com>
2021-04-22 11:16:01 +0530

    CB

---
+++ src/org/firstfloor/Datas.java
@@ -0,0 +1,44 @@
+package org.firstfloor;
+
+public class Datas {
+	
+	
+	public static void main(String[] args) {
+		
+		byte a= -128;
+		System.out.println(a);
+		
+		short b = 12345;
+		System.out.println(b);
+		
+		int c = 1234567890;
+		System.out.println(c);
+		
+		long d = 12345556677655l;
+		System.out.println(d);
+		
+		float e = 250000;
+		System.out.println(e);
+		
+		double f = 2500000000000d;
+		System.out.println(f);
+		
+		char g = 'a';
+		System.out.println(g);
+		
+		String h = "karthi";
+		System.out.println(h);
+		
+		boolean i = true;
+		System.out.println(i);
+		
+		byte a1 = 0;
+		System.out.println(a1);
+		
+		
+		
+		
+		
+	}
+
+}
--- src/org/firstfloor/TenthStandard.java
+++ src/org/firstfloor/TenthStandard.java
@@ -1,5 +1,11 @@
 package org.firstfloor;
 
 public class TenthStandard {
+	
+	public void santhan() {
+		
+		System.out.println("santhan from chennai");
+
+	}
 
 }
